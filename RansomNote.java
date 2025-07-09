18ef7cf54f452db887afde1f388e46a6ea08f604
KAVYA M <kavya77.m@gmail.com>
2025-07-09 18:40:02 +0530

    383. Ransom Note

---
+++ RansomNote.java
@@ -0,0 +1,15 @@
+//383. Ransom Note
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        for(char c : magazine.toCharArray()){
+            letters[c-'a']++;
+        }
+        for(char c : ransomNote.toCharArray()){
+            if(letters[c-'a']==0) return false;
+            letters[c-'a']--;
+        }
+        return true;
+    }
+}
