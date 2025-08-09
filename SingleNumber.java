83a59938d2b8e05ade20f9f47e6231d0a4cc0486
KAVYA M <kavya77.m@gmail.com>
2025-08-09 22:19:50 +0530

    136. Single Number

---
+++ SingleNumber.java
@@ -0,0 +1,34 @@
+//136. Single Number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNum = 0;
+        for(int i=0;i<nums.length;i++){
+            singleNum ^= nums[i];
+        }
+        return singleNum;
+    }
+}
+
+/** 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] freq = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int count = 1;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                    freq[j] = -1;
+                }
+            }
+            if(freq[i] != -1) freq[i] = count;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(freq[i] == 1) return nums[i];
+        }
+        return -1;
+    }
+}
+*/
