dda82ca4383da5cb507244e76b2b07bae8170836
KAVYA M <kavya77.m@gmail.com>
2025-09-16 23:32:24 +0530

    217. Contains Duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,24 @@
+//217. Contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+}
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int num : nums){
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
+//         for(int freq : map.values()){
+//             if(freq > 1) return true;
+//         }
+//         return false;
+//     }
+// }
