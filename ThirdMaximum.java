f1ba99e8fdebfc128ad58c1a91778b873ccdaab3
KAVYA M <kavya77.m@gmail.com>
2025-09-17 19:33:11 +0530

    414. Third Maximum Number

---
+++ ThirdMaximum.java
@@ -0,0 +1,36 @@
+//414. Third Maximum Number
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Long first, second, third;
+        first = second = third = Long.MIN_VALUE;
+        for(int num : nums){
+            long n = num;
+            if(n == first || n == second || n== third) continue;
+            if(n > first){
+                third = second;
+                second = first;
+                first = n;
+            }
+            else if(n > second){
+                third = second;
+                second = n;
+            }
+            else if(n > third){
+                third = n;
+            }
+        }
+        return third == Long.MIN_VALUE ? first.intValue() : third.intValue();
+    }
+}
+
+// class Solution {
+//     public int thirdMax(int[] nums) {
+//         TreeSet<Integer> set = new TreeSet<>();
+//         for(int num : nums){
+//             set.add(num);
+//             if(set.size() > 3) set.pollFirst();
+//         }
+//         return (set.size() < 3)? set.last() : set.first();
+//     }
+// }
