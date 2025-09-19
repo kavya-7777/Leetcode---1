9bf6c6b8af48e9b9cc6d079fbe9e7eaecdb9c978
KAVYA M <kavya77.m@gmail.com>
2025-09-19 21:32:23 +0530

    929. Unique Email Addresses

---
+++ UniqueEmailAddresses.java
@@ -0,0 +1,17 @@
+//929. Unique Email Addresses
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for(String email : emails){
+            String[] parts = email.split("@");
+            String local = parts[0];
+            String domain = parts[1];
+            if(local.contains("+")) local = local.substring(0,local.indexOf("+"));
+            local = local.replace(".","");
+            String valid = local + "@" + domain;
+            set.add(valid);
+        }
+        return set.size();
+    }
+}
