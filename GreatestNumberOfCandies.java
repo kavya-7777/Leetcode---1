61684cfbd338c831285e3881c9595026281dbe9f
KAVYA M <kavya77.m@gmail.com>
2025-09-21 19:35:10 +0530

    1431. Kids With the Greatest Number of Candies

---
+++ GreatestNumberOfCandies.java
@@ -0,0 +1,14 @@
+//1431. Kids With the Greatest Number of Candies
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>();
+        int max = 0;
+        for(int num : candies) max = Math.max(max, num);
+        for(int i=0;i<candies.length;i++){
+            if(candies[i] + extraCandies >= max) list.add(true);
+            else list.add(false);
+        }
+        return list;
+    }
+}
