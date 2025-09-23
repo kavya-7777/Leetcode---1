19791c4005ba34d2ed5daa2a91cd4d807a7401fd
KAVYA M <kavya77.m@gmail.com>
2025-09-23 09:45:08 +0530

    1528. Shuffle String

---
+++ ShuffleString.java
@@ -0,0 +1,11 @@
+//1528. Shuffle String
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] res = new char[indices.length];
+        for(int i=0;i<indices.length;i++){
+            res[indices[i]] = s.charAt(i);
+        }
+        return new String(res);
+    }
+}
