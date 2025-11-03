0f21d2a6b65b95569a49280efc5fb59106fae65c
kavya-7777 <kavya77.m@gmail.com>
2025-11-03 10:22:53 +0530

    3110. Score of a String

---
+++ ScoreOfString.java
@@ -0,0 +1,11 @@
+//3110. Score of a String
+
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for(int i=1;i<s.length();i++){
+            score += Math.abs(s.charAt(i - 1) - s.charAt(i));
+        }
+        return score;
+    }
+}
