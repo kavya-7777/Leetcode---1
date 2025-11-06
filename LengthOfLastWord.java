936d625be752df856840648b28ecef2038ae4116
kavya-7777 <kavya77.m@gmail.com>
2025-11-06 20:56:19 +0530

    58. Length of Last Word

---
+++ LengthOfLastWord.java
@@ -0,0 +1,9 @@
+//58. Length of Last Word
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] words = s.split("\\s+");
+        return words[words.length - 1].length();
+    }
+}
