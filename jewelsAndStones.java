3eafabc2bc0dffc1dea02cd5c7d902e439757dee
kavya-7777 <kavya77.m@gmail.com>
2025-11-06 19:12:09 +0530

    771. Jewels and Stones

---
+++ jewelsAndStones.java
@@ -0,0 +1,11 @@
+//771. Jewels and Stones
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(char ch : stones.toCharArray()){
+            if(jewels.indexOf(ch) != -1) count++;
+        }
+        return count;
+    }
+}
