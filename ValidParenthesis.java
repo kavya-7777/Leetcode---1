dea094152d552ee42300e22df34d2e2a0041340b
kavya-7777 <kavya77.m@gmail.com>
2025-11-21 18:53:29 +0530

    20. Valid Parentheses

---
+++ ValidParenthesis.java
@@ -0,0 +1,20 @@
+//20. Valid Parentheses
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else if(stack.isEmpty()) return false;
+            else if((ch == ')' && stack.peek() == '(') ||
+                    (ch == '}' && stack.peek() == '{') ||
+                    (ch == ']' && stack.peek() == '[')){
+                        stack.pop();
+            }
+            else return false;
+        }
+        return stack.isEmpty();
+    }
+}
